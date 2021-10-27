98a104fc64a2182ba03b53828ff4dcb6b2505c4a
Rahmi Köse <rahmikse19@gmail.com>
2021-10-27 22:11:22 +0300

    spotify-clone app

---
+++ src/Players/Player.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Player.css";
+import Sidebar from "./Sidebar/SideBar";
+import Body from "./Body/Body";
+import Footer from "./Footer/Footer";
+
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Player;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { DataLayer } from "./Context/DataLayer";
+import reducer, { initialState } from "./Context/reducer";
+import { SoundLayer } from "./Context/SoundLayer";
+import soundReducer,{soundInitialState} from "./Context/soundReducer"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <SoundLayer initialState={soundInitialState} reducer={soundReducer}>
+        <App />
+      </SoundLayer>
+    </DataLayer>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
